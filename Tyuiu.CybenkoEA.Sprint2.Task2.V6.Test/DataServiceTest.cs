12f2823d940e6cd092ecc294bbf31cbc75790783
alexe <alexe@ALEXEY52NGG>
2025-11-19 14:37:25 +0500

    Task2

---
+++ Tyuiu.CybenkoEA.Sprint2.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.CybenkoEA.Sprint2.Task2.V6.Lib;
+namespace Tyuiu.CybenkoEA.Sprint2.Task2.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckMethodCheckInDot()
+        {
+            DataService ds = new DataService();
+            int x = 15;
+            int y = 1;
+            bool res, expected;
+            res = ds.CheckDotInShadedArea(x, y);
+            expected = false;
+            Assert.AreEqual(expected, res);
+        }
+    }
+
