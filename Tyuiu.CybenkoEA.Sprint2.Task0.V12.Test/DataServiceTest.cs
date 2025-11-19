a3e87ce4a79a7d055692b4d98734b54528d5a69c
alexe <alexe@ALEXEY52NGG>
2025-11-19 14:10:00 +0500

    Task0

---
+++ Tyuiu.CybenkoEA.Sprint2.Task0.V12.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.CybenkoEA.Sprint2.Task0.V12.Lib;
+namespace Tyuiu.CybenkoEA.Sprint2.Task0.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperation()
+        {
+
+            DataService ds = new DataService();
+            int x = 1095;
+            int y = 475;
+            bool[] expected = new bool[] { false, true, false, true, false, true };
+
+
+            bool[] actual = ds.GetCompareOperations(x, y);
+
+
+            Assert.AreEqual(expected.Length, actual.Length, "Arrays do not have the same length.");
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i], $"Element at index {i} is not equal.");
+            }
+        }
+    }
+}
