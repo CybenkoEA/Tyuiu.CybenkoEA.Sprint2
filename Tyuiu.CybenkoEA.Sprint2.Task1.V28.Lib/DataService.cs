19a7ba9dd02036726ba42cb90d9c9a0e2e9dadec
alexe <alexe@ALEXEY52NGG>
2025-11-19 14:21:41 +0500

    Task1

--- Tyuiu.CybenkoEA.Sprint2.Task1.V28.Lib/DataService.cs
+++ Tyuiu.CybenkoEA.Sprint2.Task1.V28.Lib/DataService.cs
@@ -8,22 +8,28 @@ namespace Tyuiu.CybenkoEA.Sprint2.Task1.V28.Lib
         {
             bool[] res = new bool[6];
 
-            // 1. True: (a < b) = True
-            res[0] = (a < b) | ((c == d) & !(a > b)) ^ (c >= d);
+            // Выражение 1: True
+            // (a < b) = True, остальное не важно с OR
+            res[0] = (a < b) | ((c != d) & !(b <= a)) ^ (c > d);
 
-            // 2. False: (a > b) = False & ... 
-            res[1] = (a > b) & ((c != d) | !(b > a)) ^ (c < d);
+            // Выражение 2: False  
+            // (a > b) = False в AND гарантирует False
+            res[1] = (a > b) & ((c == d) | !(a < b)) ^ (d >= c);
 
-            // 3. True: (c == d) = True & (b > a) = True
+            // Выражение 3: True
+            // (c == d) = True в AND с другими True
             res[2] = (c == d) & ((b > a) | !(a == b)) ^ (d <= c);
 
-            // 4. False: (a == b) = False | ... с отрицанием чтобы получить False
+            // Выражение 4: False
+            // (a == b) = False в OR с другими False
             res[3] = (a == b) | ((c < d) & !(d == c)) ^ (b >= a);
 
-            // 5. True: (b > a) = True & (c == d) = True  
-            res[4] = (b > a) & ((c == d) | !(a >= b)) ^ (d != c);
+            // Выражение 5: True
+            // Комбинация True условий
+            res[4] = (b > a) | ((c == d) & !(a > b)) ^ (d != c);
 
-            // 6. False: (c < d) = False & ... 
+            // Выражение 6: False
+            // Противоречивые условия
             res[5] = (c < d) & ((a >= b) | !(b > a)) ^ (d == c);
 
             return res;
