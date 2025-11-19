f1c07c24fd390cd577fa339a8e99074f898d7115
alexe <alexe@ALEXEY52NGG>
2025-11-19 15:00:56 +0500

    Task5

---
+++ Tyuiu.CybenkoEA.Sprint2.Task5.V7/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.CybenkoEA.Sprint2.Task5.V7.Lib;
+namespace Tyuiu.CybenkoEA.Sprint2.Task5.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Цыбенко Е.А | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #7                                                            *");
+            Console.WriteLine("* Выполнил: Цыбенко Евгений Андреевич | ИИПБ-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите год: ");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите количество месяцев: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.FindMonthName(year, months);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
