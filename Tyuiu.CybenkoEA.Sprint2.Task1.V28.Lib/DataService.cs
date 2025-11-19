2da6591820ae83bd9a44d09381c7473e6ee5d1b2
alexe <alexe@ALEXEY52NGG>
2025-11-19 14:21:20 +0500

    Task1

--- Tyuiu.CybenkoEA.Sprint2.Task1.V28.Lib/DataService.cs
+++ Tyuiu.CybenkoEA.Sprint2.Task1.V28.Lib/DataService.cs
@@ -8,17 +8,23 @@ namespace Tyuiu.CybenkoEA.Sprint2.Task1.V28.Lib
         {
             bool[] res = new bool[6];
 
-            res[0] = (a < b) | ((c == d) & !(b > a)) ^ (c >= d);
+            // 1. True: (a < b) = True
+            res[0] = (a < b) | ((c == d) & !(a > b)) ^ (c >= d);
 
-            res[1] = (a > b) & ((c != d) | !(b <= a)) ^ (c < d);
+            // 2. False: (a > b) = False & ... 
+            res[1] = (a > b) & ((c != d) | !(b > a)) ^ (c < d);
 
-            res[2] = (c == d) & ((a != b) | !(c <= d)) ^ (b > a);
+            // 3. True: (c == d) = True & (b > a) = True
+            res[2] = (c == d) & ((b > a) | !(a == b)) ^ (d <= c);
 
-            res[3] = (a > b) | ((c != d) & !(a < b)) ^ (c == d);
+            // 4. False: (a == b) = False | ... с отрицанием чтобы получить False
+            res[3] = (a == b) | ((c < d) & !(d == c)) ^ (b >= a);
 
-            res[4] = (b > a) & ((c == d) | !(a >= b)) ^ (d >= c);
+            // 5. True: (b > a) = True & (c == d) = True  
+            res[4] = (b > a) & ((c == d) | !(a >= b)) ^ (d != c);
 
-            res[5] = (a == b) | ((c < d) & !(b > a)) ^ (c != d);
+            // 6. False: (c < d) = False & ... 
+            res[5] = (c < d) & ((a >= b) | !(b > a)) ^ (d == c);
 
             return res;
         }
