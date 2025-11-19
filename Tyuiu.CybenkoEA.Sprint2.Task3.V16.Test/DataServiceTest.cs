74caa789f2c387e3b24bad3c470702699a926226
alexe <alexe@ALEXEY52NGG>
2025-11-19 14:44:59 +0500

    Task3

---
+++ Tyuiu.CybenkoEA.Sprint2.Task3.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.CybenkoEA.Sprint2.Task3.V16.Lib;
+namespace Tyuiu.CybenkoEA.Sprint2.Task3.V16.Test
+{
+    [TestClass]
+    public class DataServiseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(128, res);
+        }
+    }
+}
---
+++ Tyuiu.CybenkoEA.Sprint2.Task3.V16/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.CybenkoEA.Sprint2.Task3.V16.Lib;
+namespace Tyuiu.CybenkoEA.Sprint2.Task3.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Цыбенко Е. А. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Цыбенко Евгений Андреевич | ИИПб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else.                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Введите значение X:                                                     *");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x));
+            Console.ReadLine();
+        }
+    }
+}
