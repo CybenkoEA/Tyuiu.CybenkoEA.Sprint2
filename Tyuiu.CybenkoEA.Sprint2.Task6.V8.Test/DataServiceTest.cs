fedda844fb1a089bf8c8ed187026728dee84ac7b
alexe <alexe@ALEXEY52NGG>
2025-11-19 15:06:32 +0500

    Task6

---
+++ Tyuiu.CybenkoEA.Sprint2.Task6.V8.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.CybenkoEA.Sprint2.Task6.V8.Lib;
+namespace Tyuiu.CybenkoEA.Sprint2.Task6.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
