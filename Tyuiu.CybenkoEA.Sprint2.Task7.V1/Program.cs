09855d4ca67fcc86f40e6086db29be30cc8af9a4
alexe <alexe@ALEXEY52NGG>
2025-11-19 15:10:31 +0500

    Task7

---
+++ Tyuiu.CybenkoEA.Sprint2.Task7.V1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.CybenkoEA.Sprint2.Task7.V1.Lib;
+namespace Tyuiu.CybenkoEA.Sprint2.Task7.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной Х:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            DataService ds = new DataService();
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
